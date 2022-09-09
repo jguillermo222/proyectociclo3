8eada75f1c367848c17a92b7bb77f822098047ed
jguillermo222 <ja.guillermo222@gmail.com>
2022-09-09 13:23:55 -0500

    first commit

---
+++ src/main/java/com/empresa/empresa2020/Empresa2020Application.java
@@ -0,0 +1,13 @@
+package com.empresa.empresa2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Empresa2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Empresa2020Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/empresa2020/Empresa2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.empresa2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Empresa2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
