577661f6059634e2a329cbcb07563470dacdf87c
jguillermo222 <ja.guillermo222@gmail.com>
2022-09-09 12:52:53 -0500

    first commit

---
+++ src/main/java/com/empresa/empresa2020/Controller/ControladorRest.java
@@ -0,0 +1,16 @@
+package com.empresa.empresa2020.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class ControladorRest {
+
+    @GetMapping("/")
+    public String saludar(){
+    return "mama";
+
+}
+}
