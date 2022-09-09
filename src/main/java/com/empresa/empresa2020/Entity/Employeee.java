1aac54aecf03b19303a72749117b6705c4cdce4b
jguillermo222 <ja.guillermo222@gmail.com>
2022-09-09 17:08:54 -0500

    4 commit

--- src/main/java/com/empresa/empresa2020/Entity/Employeee.java
+++ src/main/java/com/empresa/empresa2020/Entity/Employeee.java
@@ -1,14 +1,24 @@
 package com.empresa.empresa2020.Entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 public class Employeee {
 
-private long id;
-private String email;
-private Date updateAt;
-private Date createdAt;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) //Genera ID automatico
+    private long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @ManyToOne
+    private Enterprise enterprise;
+    private Date updateAt;
+    private Date createdAt;
+
+
+
 
 }
--- src/main/java/com/empresa/empresa2020/Entity/Enterprise.java
+++ src/main/java/com/empresa/empresa2020/Entity/Enterprise.java
@@ -2,7 +2,16 @@ package com.empresa.empresa2020.Entity;
 
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Enterprise {
+
+    @Id
+    private long id;
+
+    @OneToMany
+    private Employee employee;
+
 }
